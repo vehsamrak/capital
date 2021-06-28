28d8b0e84ea7ed232e427a8893eb91f16d53c447
Petr Karmashev <pakarmashev@avito.ru>
2021-06-28 03:28:35 +0300

    Migrations credentials moved to docker compose

--- build/package/migration-entrypoint-dev.sh
+++ build/package/migration-entrypoint-dev.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-echo Migrating database
-until /project/tools/shmig.sh -t postgresql -H database -l capitalUser -p hARdAYTEReIsUlANTanEOLIB -d capital -m /project/tools/migrations -s migrations up
+echo Database migration started
+
+until /project/tools/shmig.sh -t postgresql -H database -l ${POSTGRES_USER} -p ${POSTGRES_PASSWORD} -d capital -m /project/tools/migrations -s migrations up
 do
     echo retrying migration
     sleep 3
 done
+
+echo Database migration finished
